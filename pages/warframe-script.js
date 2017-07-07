6eb4fb341963c1262b979658bfdb705866421268
watervapour <watervapour@users.noreply.github.com>
2017-07-07 19:03:18 +1000

    wf drop work
    v.01.1

--- pages/warframe-script.js
+++ pages/warframe-script.js
@@ -1,14 +1,33 @@
 function gogogo() {
-	var searchTerm = document.getElementById("searchbar").value;
+	var searchTerm, array;
+	searchTerm = document.getElementById("searchbar").value;
+	relicArray=[];
 	
-	var table = document.getElementById("wf-table");
-	var row = table.insertRow(0);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	var cell3 = row.insertCell(2);
+	//search
+	//var dropTable = document
 	
-	cell1.innerHTML = "NEW CELL1";
-	cell2.innerHTML = searchTerm;
-	cell3.innerHTML = "NEW CELL3";
+	//fill table
+	var arrayLen = relicArray.length;
+	addRow("Item", "relic", "rarity");
+	for(i=0;i<arrayLen;i+=3){
+		addRow(relicArray[i], relicArray[i+1], relicArray[i+2])
+	}
+	return false;
+}
+
+function addRow(a, b, c){
+	var table, count, row, cell0, cell1, cell2,
+	table = document.getElementById("wf-table");
+	count = table.rows.length;
+	row = table.insertRow(count);
+	cell0 = row.insertCell(0);
+	cell1 = row.insertCell(1);
+	cell2 = row.insertCell(2);
+	
+	
+	cell0.innerHTML = a;
+	cell1.innerHTML = b;
+	cell2.innerHTML = c;
+	
 	return false;
 }
