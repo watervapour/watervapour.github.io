2f27824b4a35892eeeeed810feb22eef35a67b44
watervapour <watervapour@users.noreply.github.com>
2017-07-10 11:48:26 +1000

    a
    a

--- pages/parallax.js
+++ pages/parallax.js
@@ -1,37 +1,36 @@
 function parallax(caller){
 	//get scene center
 	var initiator = caller.id;
-	var bg = document.getElementById(initiator+"-layer-0");
+	var bg = document.getElementById(initiator+"-L0");
 	var rect = bg.getBoundingClientRect();
 	var xCent = rect.left + rect.width * 0.5;
 	var yCent = rect.top + rect.height * 0.5;
-	
+	//get mouse movement and center on image
 	var mouseX = event.clientX - xCent;
 	var mouseY = event.clientY - yCent;
 	
+	// a for loop for unknown amount of elements
 	var elsExist = true;
 	var j = 1;
 	while (elsExist == true) {
-		var layerId = initiator + "-layer-" + j;
-		if (layerId == true){
-			console.log("If entered!");
-			var layer = document.getElementById(layerId);
-			var xMultiplier = layer.data.xRange; 
-			var yMultiplier = layer.data.yRange;
-			var xInversion = layer.data.xInvert;
-			var yInversion = layer.data.yInvert;
+		// get layer 
+		var layerId = initiator + "-L" + j;
+		var layerElem = document.getElementById(layerId);
+		console.log(layerId);
+		if (layerElem){	// check is element exists
+			var xPercent = layerElem.data-xRange / 100; 
+			var yPercent = layerElem.data-yRange / 100;
+			document.getElementById("para").innerHTML = xPercent;
 			//move amount
-			var xMove = xCent + mouseX / xMultiplier * xInversion;
-			var yMove = yCent + mouseY / yMultiplier * yInversion;
+			var xMove = xCent + mouseX / xPercent;
+			var yMove = yCent + mouseY / yPercent;
 			console.log(xMove);
 			console.log(yMove);
-			layer.style.left = xMove + "px";
-			layer.style.top = yMove + "px";
+			layerElem.style.left = xMove + "px";
+			layerElem.style.top = yMove + "px";
 		} else {
 			elsExist = false;
 		}
 		j++;
 	}
-	var paraOutput = "X: " + mouseX + " | Y: " + mouseY;
-	document.getElementById("para").innerHTML = paraOutput;
 }
