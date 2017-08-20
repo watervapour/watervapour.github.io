286c8aab3fc30d401ed35d145a2c3b8ea7729e8b
watervapour <watervapour@users.noreply.github.com>
2017-08-20 21:47:52 +1000

    dynamic sidebar start
    dynamic sidebar v0.1

---
+++ pages/sidebar_experiment.js
@@ -0,0 +1,10 @@
+var contentArray = ['a','pages/sidebar_a.html',
+					'b','pages/sidebar_b.html'];
+
+var ul = document.getElementById('sidebar');					
+					
+for (i=1;i<(contentArray.length/2);i++){
+	var li = document.createElement('li');
+	
+	console.log(target);
+}	
