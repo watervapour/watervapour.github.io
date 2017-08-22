f4b541b2df26ce13bccfe26f2c73da539f35f0e9
watervapour <watervapour@users.noreply.github.com>
2017-08-22 20:14:54 +1000

    music_2 v0.2

--- pages/music_2.js
+++ pages/music_2.js
@@ -1,3 +1,35 @@
-var playmusic = function() {
+var vol = 0.5;
+var songNum = 3
+var audio = document.querySelector(`audio[data-song="${songNum}"]`);
+
+function previous() {
+  songNum -= 1;
+  alert('songNum is now '+songNum);
+  playmusic();
+}
+function next() {
+  songNum += 1;
+  alert('songNum is now '+songNum);
+  playmusic();
+}
+
+function playMusic() {
+  var audio = document.querySelector(`audio[data-song="${songNum}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  
   audio.play();
+  audio.volume = vol;
+}
+
+function volUp() {
+  vol += 0.1;
+  alert('The volume is now '+vol);
+  audio.volume = vol;
+}
+
+function voldDown() { 
+  vol -= 0.1;
+  alert('The volume is now '+vol);
+  audio.volume = vol;
 }
