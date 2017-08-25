d6e9e6847fbc6563c17f609eec4e121dc6148f40
watervapour <watervapour@users.noreply.github.com>
2017-08-25 22:25:39 +1000

    music 2 work
    its now broken yet more complete

--- pages/music_2.js
+++ pages/music_2.js
@@ -1,35 +1,65 @@
-var vol = 0.5;
-var songNum = 3
-var audio = document.querySelector(`audio[data-song="${songNum}"]`);
+function init() {
+	console.log('init');
+  // controls if need be
+  const buttonPrev = document.getElementById("buttonPrev");
+  const buttonPlay = document.getElementById("buttonPlay");
+  const buttonNext = document.getElementById("buttonNext");
+  const buttonVolU = document.getElementById("buttonVolU");
+  const buttonVolD = document.getElementById("buttonVolD");
+  
+  var vol = 0.5;
+  var track = 3
+  var song = document.querySelector(`audio[data-song="${track}"]`);
+  var paused = true;
+}
+init();
+
+function setTrack(clicked){
+  song.pause();
+  track = clicked.dataset.track;
+  playMusic();
+}
 
 function previous() {
-  songNum -= 1;
-  alert('songNum is now '+songNum);
+  song.pause();
+  track -= 1;
+  alert('track is now '+track);
+  song.current
   playmusic();
 }
 function next() {
-  songNum += 1;
-  alert('songNum is now '+songNum);
+  song.pause();
+  track += 1;
+  alert('track is now '+track);
   playmusic();
 }
 
+function togglePause(){
+  paused = !paused;
+  if(paused === true) {
+	buttonPlay.value = "Play";  
+  } else {
+	buttonPlay.value = "Pause";
+    song.play();	
+  }
+}
+
 function playMusic() {
-  var audio = document.querySelector(`audio[data-song="${songNum}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  
-  audio.play();
-  audio.volume = vol;
+  var song = document.querySelector(`audio[data-song="${track}"]`);
+  if(!song) return;
+  song.currentTime = 0;
+  song.play();
+  song.volume = vol;
 }
 
 function volUp() {
   vol += 0.1;
   alert('The volume is now '+vol);
-  audio.volume = vol;
+  song.volume = vol;
 }
 
 function voldDown() { 
   vol -= 0.1;
   alert('The volume is now '+vol);
-  audio.volume = vol;
+  song.volume = vol;
 }
